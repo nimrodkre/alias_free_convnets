17b5a8e87df7b5519ea3616fb233c3d805541322
nimrodkre <nimrod.kremer@mail.huji.ac.il>
2023-03-30 09:48:24 +0300

    Added the code for the weights

--- run_models.py
+++ run_models.py
@@ -1,13 +1,13 @@
 import torch
 from utils import get_args, get_model
 
-WEIGHTS_PATH = r'C:\HUJI\lab_weiss\alias_free_convnets\alias_free_convnets\convnext_afc_tiny_ideal_up_poly_per_channel_scale_7_7_chw2_stem_mode_lpf_poly_cutoff0.75\checkpoint-best-ema.pth'
+WEIGHTS_PATH = r'C:\HUJI\lab_weiss\alias_free_convnets\alias_free_convnets\convnext_aps_tiny_f1_gelu_c_s2\checkpoint-best-ema.pth'
 
 def main():
     args = get_args()
     model = get_model(args=args)
     checkpoint = torch.load(WEIGHTS_PATH,map_location=torch.device('cpu'))
-    model.load_state_dict(checkpoint, strict=False)
+    model.load_state_dict(checkpoint["model"], strict=True)
     model.eval()
     a = 5
 
